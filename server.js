b77eb2b38d9a82976e11ec37c0b32b07cbae83b8
raju-ahammad <web.rajuahamad@gmail.com>
2021-06-29 03:34:46 +0600

    setup dougact

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+var fs = require("fs");
+
+const app = express();
+
+
+// ***************** use middleware *****************
+
+app.use(express.json());
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// *************** Routes **********************
+
+//app.use('/user', require('./routes/userRouter'))
+
+app.get('/', (req, res) => {
+    res.send('Hello Dougcat!')
+  })
+
+// *************** connect mongodb **********************
+// const URI = process.env.MONGO_URI;
+
+
+// mongoose.connect(URI, {
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, err => {
+//     if(err) throw err;
+//     console.log("connected to mongo db");
+  
+// })
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on ${PORT}`)
+})

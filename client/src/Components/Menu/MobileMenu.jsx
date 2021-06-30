6e6d03be922cf281a3955544ff64cc2d1e00f3e7
raju-ahammad <web.rajuahamad@gmail.com>
2021-06-30 10:48:51 +0600

    update clipath

--- client/src/Components/Menu/MobileMenu.jsx
+++ client/src/Components/Menu/MobileMenu.jsx
@@ -4,10 +4,10 @@ import "./MobileMenu.css"
 const MobileMenu = () => {
     return (
         <div className="MobileMenu"> 
-            <h2><Link to="/">HOME</Link></h2>
-            <h2><Link to="/work">WORK</Link></h2>
-            <h2><Link to="/blog">BLOG</Link></h2>
-            <h2><Link to="/company">COMPANY</Link></h2>
+            <h2><Link onClick={()=>{window.location.href = '/'}} to="/">HOME</Link></h2>
+            <h2><Link onClick={()=>{window.location.href = '/work'}} to="/work">WORK</Link></h2>
+            <h2><Link onClick={()=>{window.location.href = '/blog'}} to="/blog">BLOG</Link></h2>
+            <h2><Link onClick={()=>{window.location.href = '/company'}} to="/company">COMPANY</Link></h2>
 
         </div>
     )

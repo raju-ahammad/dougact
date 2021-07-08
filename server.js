504f244ea874009c97c939c06f503c5f086bdcca
raju-ahammad <web.rajuahamad@gmail.com>
2021-07-08 23:25:57 +0600

    version 1.0.0

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser')
 const passport = require("passport");
+const path = require('path')
 var fs = require("fs");
 
 
@@ -48,6 +49,17 @@ mongoose.connect(URI, {
   
 })
 
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
 const PORT = process.env.PORT || 5000;

6273eb8a2c784b6095fafadd397080b91ac272f6
raju-ahammad <web.rajuahamad@gmail.com>
2021-08-09 23:36:15 +0600

    update server

--- server.js
+++ server.js
@@ -29,12 +29,6 @@ require("./utils/passport")(passport);
 
 
 
-// *************** Routes **********************
-
-  app.get('', function (req, res) {
-      res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-  });
-
 
 
 
@@ -67,14 +61,14 @@ mongoose.connect(URI, {
 
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+
     // Set static folder
     app.use(express.static('client/build'));
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+
 
 
 const PORT = process.env.PORT || 5000;
